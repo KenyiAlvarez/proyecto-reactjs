ba140f3253f1dbfbe6bf804999a642ed7c34eb80
KenyiAlvarez <kenyifullstack@gamil.com>
2022-05-24 21:24:26 -0300

    agregue Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from './components/Navbar/Navbar'
+import Inicio from './components/Pages/Inicio'
+import Items from './components/Pages/Items'
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Inicio/>}/>
+          <Route exact path="/Items" element={<Items/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
