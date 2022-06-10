d1beb367da7358dbdcb7c9107ffabc14c3728697
KenyiAlvarez <kenyifullstack@gamil.com>
2022-06-10 09:49:50 -0300

    arreglo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Style/style.css"
-import { Nav } from "./components/NavBar/NavBar"
+import { Nav } from "./components/NavBar/NavBar";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer"
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
